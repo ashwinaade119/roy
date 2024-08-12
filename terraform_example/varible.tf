fa0853e5a837cea5c42cfed1b9b62aa8af973a5c
ashu <ashwinaade1119@gmail.com>
2024-08-12 18:30:27 +0530

    --- Auto Git Commit ---

--- terraform_example/varible.tf
+++ terraform_example/varible.tf
@@ -27,32 +27,6 @@ variable "value" {
   type = string
   description = "output block calling"
 }
-variable "region" {
-  type = string
-  description = "region calling"
-}
 
-variable "ami" {
-  type = string
-  description = "ami calling"
-}
 
-variable "instance_type" {
-  type = string
-  description = "server size calling"
-}
 
-variable "key_name" {
-  type = string
-  description = "key calling"
-}
-
-variable "tags" {
-  type = map
-  description = "calling the tag here"
-}
-
-variable "value" {
-  type = string
-  description = "output block calling"
-}
