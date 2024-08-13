9e0482ea474cc82f6954b41e71bb8f5467e70dd1
ashu <ashwinaade1119@gmail.com>
2024-08-13 17:55:22 +0530

    --- Auto Git Commit ---

--- terraform_example/main.tf
+++ terraform_example/main.tf
@@ -11,3 +11,13 @@ provider "aws" {
   # Configuration options
   region = var.region
 }
+#### Try to create one ubuntu server
+resource "aws_instance" "this" {
+  ami                     = var.ami
+  instance_type           = var.instance_type
+  key_name                = var.key_name
+
+  tags                    = var.tags
+
+}
+
