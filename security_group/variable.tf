0708c56b60d11b347da5403c3b1c8d95e89f2db9
ashu <ashwinaade1119@gmail.com>
2024-08-22 11:23:11 +0530

    --- Auto Git Commit ---

---
+++ security_group/variable.tf
@@ -0,0 +1,44 @@
+variable "name" {
+  description = "Name of the security group"
+  type        = string
+}
+
+variable "description" {
+  description = "Description of the security group"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "The VPC ID where the security group will be created"
+  type        = string
+}
+
+variable "ingress_rules" {
+  description = "List of ingress rules"
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+    description = optional(string)
+  }))
+  default = []
+}
+
+variable "egress_rules" {
+  description = "List of egress rules"
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+    description = optional(string)
+  }))
+  default = []
+}
+
+variable "tags" {
+  description = "A map of tags to assign to the resource"
+  type        = map(string)
+  default     = {}
+}
