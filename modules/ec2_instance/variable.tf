d355a5525e3b6967503e8711574f70ad34ad3e68
ashu <ashwinaade1119@gmail.com>
2024-08-24 19:30:27 +0530

    --- Auto Git Commit ---

--- modules/ec2_instance/variable.tf
+++ modules/ec2_instance/variable.tf
@@ -18,10 +18,6 @@ variable "vpc_security_group_ids" {
   type        = list(string)
 }
 
-variable "user_data_file" {
-  description = "The path to the user data script file"
-  type        = string
-}
 
 variable "tags" {
   description = "A map of tags to assign to the resource"
