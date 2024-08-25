f703f20db574ebe018705d42e3c5cba4f1762fff
ashu <ashwinaade1119@gmail.com>
2024-08-25 18:02:04 +0530

    --- Auto Git Commit ---

--- s3_bucket/s3_main.tf
+++ s3_bucket/s3_main.tf
@@ -1,24 +1,24 @@
-# Create a S3 Bucket for state locking
+ Create a S3 Bucket for state locking
 
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#     }
-#   }
-# }
+ terraform {
+   required_providers {
+     aws = {
+       source  = "hashicorp/aws"
+     }
+   }
+ }
 
-# provider "aws" {
-#   region = "us-west-2"
-# }
+ provider "aws" {
+   region = "us-west-2"
+ }
 
-# resource "aws_s3_bucket" "terraform-state" {
-#   bucket = "terraform-state-dcube"
-# }
+ resource "aws_s3_bucket" "terraform-state" {
+   bucket = "terraform-state-dcube"
+ }
 
-# resource "aws_s3_bucket_versioning" "terraform-state" {
-#   bucket = aws_s3_bucket.terraform-state.id
-#   versioning_configuration {
-#     status = "Enabled"
-#   }
-# }
+ resource "aws_s3_bucket_versioning" "terraform-state" {
+   bucket = aws_s3_bucket.terraform-state.id
+   versioning_configuration {
+     status = "Enabled"
+   }
+ }
