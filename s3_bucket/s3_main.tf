14aeff1bbe7cc6226a9d40e74e4f4f2adf30fd96
ashu <ashwinaade1119@gmail.com>
2024-08-25 18:10:47 +0530

    --- Auto Git Commit ---

--- s3_bucket/s3_main.tf
+++ s3_bucket/s3_main.tf
@@ -11,7 +11,7 @@ terraform {
  }
 
  resource "aws_s3_bucket" "terraform-state" {
-   bucket = "terraform-state-dcube"
+   bucket = "terraform-state-dcube-220"
  }
 
  resource "aws_s3_bucket_versioning" "terraform-state" {
