c8bbf01c18437359c5b4c3db54d2af17f6c551fb
ashu <ashwinaade1119@gmail.com>
2024-08-26 19:28:58 +0530

    --- Auto Git Commit ---

--- terraform_example/main.tf
+++ terraform_example/main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 }
 
 ##### Try to create one ubuntu server
-resource "aws_instance" "this" {
+resource "aws_instance" "ashwin1" {
   ami                     = var.ami
   instance_type           = var.instance_type
   key_name                = var.key_name
